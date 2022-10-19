1d969f0564994861a1313048f45f700b7a480f5f
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-10-19 14:08:05 +0900

    Create Vector<Integer> 컬렉션 사용.java

---
+++ Ch7.제네릭_컬렉션/Vector<Integer> 컬렉션 사용.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class VectorEx {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		Vector<Integer> v=new Vector<Integer>();
+		
+		v.add(5);
+		v.add(4);
+		v.add(-1);
+		
+		v.add(2, 100);
+		
+		System.out.println("벡터의 요소의 개수 : " + v.size());
+		System.out.println("벡터의 용량 : " + v.capacity());
+		
+		for(int i=0; i<v.size(); i++) {
+			int n=v.get(i);
+			System.out.println(n);
+		}
+		
+		int sum=0;
+		
+		for(int i=0; i<v.size(); i++) {
+			int n=v.get(i);
+			sum+=n;
+		}
+		System.out.println("벡터에 있는 정수 합 : "+ sum);
+	}
+
+}
